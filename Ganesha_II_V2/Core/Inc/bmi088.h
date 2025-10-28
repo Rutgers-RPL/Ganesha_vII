3bd1d12e1724a9bfae8934f05b2d987e9e2e1563
dmanslick <dvvshah@gmail.com>
2025-10-28 19:15:04 -0400

    started working on code for bmi

---
+++ Ganesha_II_V2/Core/Inc/bmi088.h
@@ -0,0 +1,15 @@
+/*
+ * bmi088.h
+ *
+ *  Created on: Oct 26, 2025
+ *      Author: Dhruv Shah
+ */
+
+#ifndef INC_BMI088_H_
+#define INC_BMI088_H_
+
+#include "bmi08x.h"
+
+
+
+#endif /* INC_BMI088_H_ */
---
+++ Ganesha_II_V2/Core/Src/bmi088.c
@@ -0,0 +1,60 @@
+/*
+ * bmi088.c
+ *
+ *  Created on: Oct 26, 2025
+ *      Author: Dhruv Shah
+ */
+
+#include "bmi08_defs.h"
+#include <stdint.h>
+
+BMI08_INTF_RET_TYPE read_spi(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr) {
+	SPI_HandleTypeDef* handle = (SPI_HandleTypeDef*) intf_ptr;
+	return HAL_SPI_Receive(handle, reg_data, (uint16_t)len, HAL_MAX_DELAY);
+}
+
+BMI08_INTF_RET_TYPE write_spi(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr) {
+	SPI_HandleTypeDef* handle = (SPI_HandleTypeDef*) intf_ptr;
+	return HAL_SPI_Transmit(handle, reg_data, (uint16_t)len, HAL_MAX_DELAY);
+}
+
+void delay(uint32_t period, void *intf_ptr) {
+	HAL_Delay(period*1000);
+};
+
+int8_t bmi088_init(struct bmi08_dev* bmi, SPI_HandleTypeDef* accelerometer_handle, SPI_HandleTypeDef* gyro_handle) {
+	bmi->intf_ptr_accel = accelerometer_handle;
+	bmi->intf_ptr_gyro = gyro_handle;
+	bmi->intf = BMI08_SPI_INTF;
+	bmi->variant = BMI088_VARIANT;
+	bmi->dummy_byte = 0x8e;
+	bmi->accel_cfg;
+	bmi->gyro_cfg;
+	bmi->config_file_ptr = &bmi08x_config_file;
+	bmi->read_write_len = 64;
+	bmi->read = read_spi;
+	bmi->write = write_spi;
+	bmi->delay_us = delay;
+
+	int8_t result_g = bmi08g_init(bmi);
+
+	if (result_g != BMI08_OK) {
+		return result_g;
+	}
+
+	int8_t result_a = bmi08a_init(bmi);
+
+	if (result_a != BMI08_OK) {
+		return result_a;
+	}
+
+	return 0;
+}
+
+int8_t bmi088_get_acceleration_data(struct bmi08_dev* bmi, bmi08_sensor_data* data) {
+	return bmi08a_get_data(data, bmi);
+};
+
+int8_t bmi088_get_gyro_data(struct bmi08_dev* bmi, bmi08_sensor_data* data) {
+	return bmi08g_get_data(data, bmi);
+};

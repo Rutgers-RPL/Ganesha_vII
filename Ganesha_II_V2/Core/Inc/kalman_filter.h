ba6b2e2fbdcd1a4a979c922264ce99d711a80092
dmanslick <dvvshah@gmail.com>
2026-04-03 15:48:16 -0400

    Added matricies to the kalman filter instance struct, still need to implement the update function

--- Ganesha_II_V2/Core/Inc/kalman_filter.h
+++ Ganesha_II_V2/Core/Inc/kalman_filter.h
@@ -11,16 +11,35 @@
 #include "arm_math.h"
 
 struct Kalman_Filter_Instance {
-	float32_t accel_quaternion[4];
-	float32_t orientation_quaternion[4];
-	float32_t barometer_reading;
-	float32_t initial_altitude;
-	float32_t altitude_estimate;
+    float32_t x_data[2];
+    float32_t P_data[4];
+    float32_t F_data[4];
+    float32_t Q_data[4];
+    float32_t H_data[2];
+    float32_t R_data[1];
+    float32_t K_data[2];
+    float32_t y_data[1];
+    float32_t S_data[1];
+
+    arm_matrix_instance_f32 x;
+    arm_matrix_instance_f32 P;
+    arm_matrix_instance_f32 F;
+    arm_matrix_instance_f32 Q;
+    arm_matrix_instance_f32 H;
+    arm_matrix_instance_f32 R;
+    arm_matrix_instance_f32 K;
+    arm_matrix_instance_f32 y;
+    arm_matrix_instance_f32 S;
+
+    float32_t dt;
+    float32_t barometric_altitude;
+    float32_t accel_quaternion[4];
+    float32_t orientation_quaternion[4];
 };
 
-void kalman_filter_init(struct Kalman_Filter_Instance* instance, float32_t initial_altitude);
+void kalman_filter_init(struct Kalman_Filter_Instance* instance);
 void kalman_filter_update(struct Kalman_Filter_Instance* instance);
-void kalman_filter_update_barometer(struct Kalman_Filter_Instance* instance, float32_t reading);
+void kalman_filter_update_barometric_altitude(struct Kalman_Filter_Instance* instance, float32_t barometric_altitude);
 void kalman_filter_update_accel(struct Kalman_Filter_Instance* instance, float32_t x, float32_t y, float32_t z);
 void kalman_filter_update_orientation(struct Kalman_Filter_Instance* instance, float32_t w, float32_t x, float32_t y, float32_t z);
 float32_t kalman_filter_get_altitude_estimate(struct Kalman_Filter_Instance* instance);
--- Ganesha_II_V2/Core/Src/kalman_filter.c
+++ Ganesha_II_V2/Core/Src/kalman_filter.c
@@ -8,22 +8,58 @@
 #include "kalman_filter.h"
 #include "arm_math.h"
 
-void kalman_filter_init(struct Kalman_Filter_Instance* instance, float32_t initial_altitude) {
-	float32_t empty_quaternion = {0.0, 0.0, 0.0, 0.0};
+void kalman_filter_init(struct Kalman_Filter_Instance* instance, float32_t dt) {
+    instance->dt = dt;
 
-	memcpy(instance->accel_quaternion, empty_quaternion, sizeof(empty_quaternion));
-	memcpy(instance->orientation_quaternion, empty_quaternion, sizeof(empty_quaternion));
+    instance->x_data[0] = 0.0f;
+    instance->x_data[1] = 0.0f;
 
-	instance->initial_altitude = initial_altitude;
-	instance->barometer_reading = 0;
+    instance->P_data[0] = 1.0f;
+    instance->P_data[1] = 0.0f;
+    instance->P_data[2] = 0.0f;
+    instance->P_data[3] = 1.0f;
+
+    instance->F_data[0] = 1.0f;
+    instance->F_data[1] = dt;
+    instance->F_data[2] = 0.0f;
+    instance->F_data[3] = 1.0f;
+
+    float32_t q_alt = 0.01f;
+    float32_t q_vel = 0.1f;
+    instance->Q_data[0] = q_alt;
+    instance->Q_data[1] = 0.0f;
+    instance->Q_data[2] = 0.0f;
+    instance->Q_data[3] = q_vel;
+
+    instance->H_data[0] = 1.0f;
+    instance->H_data[1] = 0.0f;
+
+    instance->R_data[0] = 0.5f;
+
+    arm_mat_init_f32(&instance->x, 2, 1, instance->x_data);
+    arm_mat_init_f32(&instance->P, 2, 2, instance->P_data);
+    arm_mat_init_f32(&instance->F, 2, 2, instance->F_data);
+    arm_mat_init_f32(&instance->Q, 2, 2, instance->Q_data);
+    arm_mat_init_f32(&instance->H, 1, 2, instance->H_data);
+    arm_mat_init_f32(&instance->R, 1, 1, instance->R_data);
+    arm_mat_init_f32(&instance->K, 2, 1, instance->K_data);
+    arm_mat_init_f32(&instance->y, 1, 1, instance->y_data);
+    arm_mat_init_f32(&instance->S, 1, 1, instance->S_data);
+
+    instance->barometer_reading = 0.0f;
+
+    for (int i = 0; i < 4; i++) {
+        instance->accel_quaternion[i] = 0.0f;
+        instance->orientation_quaternion[i] = (i==0) ? 1.0f : 0.0f;
+    }
 }
 
 void kalman_filter_update(struct Kalman_Filter_Instance* instance) {
 
 }
 
-void kalman_filter_update_barometer(struct Kalman_Filter_Instance* instance, float32_t reading) {
-	instance->barometer_reading = reading;
+void kalman_filter_update_barometric_altitude(struct Kalman_Filter_Instance* instance, float32_t barometric_altitude) {
+	instance->barometric_altitude = barometric_altitude;
 }
 
 void kalman_filter_update_accel(struct Kalman_Filter_Instance* instance, float32_t x, float32_t y, float32_t z) {
@@ -41,5 +77,5 @@ void kalman_filter_update_orientation(struct Kalman_Filter_Instance* instance, f
 }
 
 float32_t kalman_filter_get_altitude_estimate(struct Kalman_Filter_Instance* instance) {
-	return instance->altitude_estimate;
+	return instance->x_data[0];
 }

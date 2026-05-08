aa2621d21499862df39b37e89157a425f99b184e
dmanslick <dvvshah@gmail.com>
2026-05-08 17:57:12 -0400

    Created a dumb timer using HAL_GetTick to return true after a set amount of time, basic API to do some basic things

---
+++ Ganesha_II_V2/Core/Src/dumb_timer.h
@@ -0,0 +1,27 @@
+/*
+ * dumb_timer.h
+ *
+ *  Created on: May 8, 2026
+ *      Author: Dhruv'
+ */
+
+#ifndef SRC_DUMB_TIMER_H_
+#define SRC_DUMB_TIMER_H_
+
+#include <stdint.h>
+
+#define SEC_TO_MS(sec) ((sec) * 1000)
+#define MIN_TO_MS(min) (SEC_TO_MS((min) * 60))
+
+struct Dumb_Timer {
+	uint32_t start_time_ms;
+	uint32_t time_ms;
+	uint8_t started;
+};
+
+void dumb_timer_init(struct Dumb_Timer* timer, uint32_t time_ms);
+void dumb_timer_start(struct Dumb_Timer* timer);
+void dumb_timer_clear(struct Dumb_Timer* timer);
+uint8_t dumb_timer_done(struct Dumb_Timer* timer);
+
+#endif /* SRC_DUMB_TIMER_H_ */
